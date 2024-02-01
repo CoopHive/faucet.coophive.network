a9b075e9f34984c0913958396fe30c5408f5ce1a
Hiro <laciferin@gmail.com>
2024-02-02 05:22:58 +0530

    server: config

--- cmd/server.go
+++ cmd/server.go
@@ -34,23 +34,53 @@ var PORT = func() int {
 	}
 	return p
 }()
+var PROXY_COUNT = func() int {
+	p, err := strconv.Atoi(GetFromEnv("PROXY_COUNT", "0"))
+	if err != nil {
+		return 0
+	}
+	return p
+}()
+
+var FAUCET_ETHER_AMOUNT = func() int {
+	p, err := strconv.Atoi(GetFromEnv("PROXY_COUNT", "1"))
+	if err != nil {
+		return 0
+	}
+	return p
+}()
+var FAUCET_TOKEN_AMOUNT = func() int {
+	p, err := strconv.Atoi(GetFromEnv("PROXY_COUNT", "1"))
+	if err != nil {
+		return 1
+	}
+	return p
+}()
+
+var FAUCET_INTERVAL = func() int {
+	p, err := strconv.Atoi(GetFromEnv("FAUCET_INTERVAL", "1440"))
+	if err != nil {
+		return 1440
+	}
+	return p
+}
 
 var (
 	appVersion = "v0.1.0"
 	chainIDMap = map[string]int{"goerli": 5, "sepolia": 11155111, "calib": 314159, "fvm": 314}
 
 	httpPortFlag = flag.Int("httpport", PORT, "Listener port to serve HTTP connection")
-	proxyCntFlag = flag.Int("proxycount", 0, "Count of reverse proxies in front of the server")
-	versionFlag  = flag.Bool("version", false, "Print version number")
+	proxyCntFlag = flag.Int("proxycount", PROXY_COUNT, "Count of reverse proxies in front of the server")
+	versionFlag  = flag.Bool("version", true, "Print version number")
 
-	payoutFlag       = flag.Int("faucet.amount", 1, "Number of Ethers to transfer per user request")
-	payoutTokensFlag = flag.Int("faucet.tokenamount", 1, "Number of Tokens to transfer per user request")
-	intervalFlag     = flag.Int("faucet.minutes", 1440, "Number of minutes to wait between funding rounds")
-	netnameFlag      = flag.String("faucet.name", "testnet", "Network name to display on the frontend")
-	symbolFlag       = flag.String("faucet.symbol", "ETH", "Token symbol to display on the frontend")
+	payoutFlag       = flag.Int("faucet.amount", FAUCET_ETHER_AMOUNT, "Number of Ethers to transfer per user request")
+	payoutTokensFlag = flag.Int("faucet.tokenamount", FAUCET_TOKEN_AMOUNT, "Number of Tokens to transfer per user request")
+	intervalFlag     = flag.Int("faucet.minutes", FAUCET_INTERVAL(), "Number of minutes to wait between funding rounds")
+	netnameFlag      = flag.String("faucet.name", GetFromEnv("FAUCET_NAME", "CoopHive v0 Testnet"), "Network name to display on the frontend")
+	symbolFlag       = flag.String("faucet.symbol", GetFromEnv("FAUCET_SYMBOL", "HIVE"), "Token symbol to display on the frontend")
 
-	keyJSONFlag  = flag.String("wallet.keyjson", os.Getenv("KEYSTORE"), "Keystore file to fund user requests with")
-	keyPassFlag  = flag.String("wallet.keypass", "password.txt", "Passphrase text file to decrypt keystore")
+	keyJSONFlag  = flag.String("wallet.keyjson", GetFromEnv("KEYSTORE", ""), "Keystore file to fund user requests with")
+	keyPassFlag  = flag.String("wallet.keypass", GetFromEnv("KEYSTORE_PASS", "password.txt"), "Passphrase text file to decrypt keystore")
 	privKeyFlag  = flag.String("wallet.privkey", os.Getenv("PRIVATE_KEY"), "Private key hex to fund user requests with")
 	providerFlag = flag.String("wallet.provider", os.Getenv("WEB3_PROVIDER"), "Endpoint for Ethereum JSON-RPC connection")
 	tokenAddress = flag.String("wallet.tokenaddress", os.Getenv("TOKEN_ADDRESS"), "Address of ERC-20 token contract")
