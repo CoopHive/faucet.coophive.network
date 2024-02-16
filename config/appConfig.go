27acc332ef5375ab11b1b65f3a194a588e038d1d
Hiro <laciferin@gmail.com>
2024-02-16 21:39:42 +0530

    appConfig: enums erflect
    Signed-off-by: Hiro <laciferin@gmail.com>

--- config/appConfig.go
+++ config/appConfig.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strconv"
 
 	"github.com/CoopHive/faucet.coophive.network/enums"
 )
@@ -35,6 +36,55 @@ var appConfig = configMap[string]{
 	enums.WEB3_PRIVATE_KEY: {
 		"private key",
 		"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+	},
+
+	enums.FAUCET_AMOUNT: {
+		desc:       "Number of Ethers to transfer per user request",
+		defaultVal: FAUCET_ETHER_AMOUNT,
+	},
+	enums.FAUCET_TOKENAMOUNT: {
+		desc:       "Number of Tokens to transfer per user request",
+		defaultVal: FAUCET_TOKEN_AMOUNT,
+	},
+	enums.FAUCET_MINUTES: {
+		desc:       "Number of minutes to wait between funding rounds",
+		defaultVal: strconv.Itoa(FaucetInterval()),
+	},
+	enums.FAUCET_NAME: {
+		desc:       "Network name to display on the frontend",
+		defaultVal: GetFromEnv("FAUCET_NAME", "CALIBRATION"),
+	},
+	enums.FAUCET_SYMBOL: {
+		desc:       "Token symbol to display on the frontend",
+		defaultVal: GetFromEnv("FAUCET_SYMBOL", "HIVE"),
+	},
+	enums.WALLET_KEYJSON: {
+		desc:       "Keystore file to fund user requests with",
+		defaultVal: GetFromEnv("KEYSTORE", ""),
+	},
+	enums.WALLET_KEYPASS: {
+		desc:       "Passphrase text file to decrypt keystore",
+		defaultVal: GetFromEnv("KEYSTORE_PASS", "password.txt"),
+	},
+	enums.WALLET_PRIVKEY: {
+		desc:       "Private key hex to fund user requests with",
+		defaultVal: os.Getenv("PRIVATE_KEY"),
+	},
+	enums.WALLET_PROVIDER: {
+		desc:       "Endpoint for Ethereum JSON-RPC connection",
+		defaultVal: os.Getenv("WEB3_PROVIDER"),
+	},
+	enums.WALLET_TOKENADDRESS: {
+		desc:       "Address of ERC-20 token contract",
+		defaultVal: os.Getenv("TOKEN_ADDRESS"),
+	},
+	enums.HCAPTCHA_SITEKEY: {
+		desc:       "hCaptcha sitekey",
+		defaultVal: os.Getenv("HCAPTCHA_SITEKEY"),
+	},
+	enums.HCAPTCHA_SECRET: {
+		desc:       "hCaptcha secret",
+		defaultVal: os.Getenv("HCAPTCHA_SECRET"),
 	},
 }
 
